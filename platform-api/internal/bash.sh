6cc42c4380a8c0795b2451cd3a66ef43a2f7ee57
marcos <marcosalbano16@hotmail.com>
2026-07-31 15:56:56 +0000

    chore: prepare project structure for authentication

---
+++ platform-api/internal/bash.sh
@@ -0,0 +1,14 @@
+git checkout -b feature/basic-auth
+
+
+mkdir -p internal/auth
+mkdir -p internal/audit
+mkdir -p internal/middleware
+mkdir -p internal/models
+mkdir -p configs
+mkdir logs
+
+
+git add .
+
+git commit -m "chore: prepare project structure for authentication"
